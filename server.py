8837d8a5038c7daaf5dd8269deacaa0dee9f916f
RaymondBernard <ray.bernard@outlook.com>
2023-07-19 14:55:46 -0400

    updated code for sql prompt engineering

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import os
-
 import openai
 from dotenv import load_dotenv
 from flask import Flask, render_template, request
@@ -9,23 +8,42 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/get_response")
+@app.route("/get_response", methods=['GET', 'POST'])
 def get_response():
-    message = request.args.get("message")
-    completion = openai.ChatCompletion.create(
-        # You can switch this to `gpt-4` if you have access to that model.
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": message}],
-    )
-    response = completion["choices"][0]["message"]["content"]
-    return response
-
+    if request.method == 'POST':
+        message = request.form.get("message")
+    else:  # This handles the GET request case
+        message = request.args.get("message")
+        
+    if 'sql' in message.lower():
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Given the following SQL tables, your job is to write queries given a user’s request.\n\nCREATE TABLE Orders (\n  OrderID int,\n  CustomerID int,\n  OrderDate datetime,\n  OrderTime varchar(8),\n  PRIMARY KEY (OrderID)\n);\n\nCREATE TABLE OrderDetails (\n  OrderDetailID int,\n  OrderID int,\n  ProductID int,\n  Quantity int,\n  PRIMARY KEY (OrderDetailID)\n);\n\nCREATE TABLE Products (\n  ProductID int,\n  ProductName varchar(50),\n  Category varchar(50),\n  UnitPrice decimal(10, 2),\n  Stock int,\n  PRIMARY KEY (ProductID)\n);\n\nCREATE TABLE Customers (\n  CustomerID int,\n  FirstName varchar(50),\n  LastName varchar(50),\n  Email varchar(100),\n  Phone varchar(20),\n  PRIMARY KEY (CustomerID)\n);"
+                },
+                {
+                    "role": "user",
+                    "content": message
+                }
+            ],
+            temperature=0,
+            max_tokens=1000,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+    else:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": message}],
+        )
+    response_content = response["choices"][0]["message"]["content"]
+    return response_content
 
 if __name__ == "__main__":
     app.run(debug=True)
